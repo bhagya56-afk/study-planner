bbe90366a5f61fe44d388a364d1a229cf4ab1f16
bhagya56-afk <sanemostly116@gmail.com>
2026-06-20 11:17:36 +0530

    Day 1 html file added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "<h1>Welcome to Study Planner</h1>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
