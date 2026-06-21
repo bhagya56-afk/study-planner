70c0dbf095581eb4adcb89379b0298af0969765e
bhagya56-afk <sanemostly116@gmail.com>
2026-06-21 10:59:10 +0530

    Day 2 Add task functionality

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('index.html')
+
+    if request.method == "POST":
+        task = request.form["task"]
+        tasks.append(task)
+
+    return render_template(
+        "index.html",
+        tasks=tasks
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
