2c27e05f102964580188352f97fed768b517cd09
bhagya56-afk <sanemostly116@gmail.com>
2026-06-21 11:02:19 +0530

    Day 3 Added delete feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -11,10 +11,12 @@ def home():
         task = request.form["task"]
         tasks.append(task)
 
-    return render_template(
-        "index.html",
-        tasks=tasks
-    )
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    tasks.pop(index)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
